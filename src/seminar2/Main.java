1be78f853a4c428b24bdb33175f46a4a06c44429
Dzmitry Liashko <dzliashko@gmail.com>
2023-08-04 19:48:47 +0300

    Feat: seminar 2 finished

--- src/seminar2/Main.java
+++ src/seminar2/Main.java
@@ -1,4 +1,13 @@
 package seminar2;
 
+import java.util.Arrays;
+
+import static seminar2.HeapSort.*;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 0, 2, 9, 3, 8, 4, 7, 6, 5};
+        heapSort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
