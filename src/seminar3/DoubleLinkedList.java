669a2c2d1dc236ba59a0beee729453d436184dfd
diniles <dzliashko@gmail.com>
2023-08-22 15:13:02 +0300

    Feat: seminar 3 class DoubleLinkedList reverse method

--- src/seminar3/DoubleLinkedList.java
+++ src/seminar3/DoubleLinkedList.java
@@ -34,6 +34,21 @@ public class DoubleLinkedList<T> {
         }
     }
 
+    public void reverse() {
+        Node<T> current = head;
+        Node<T> temp = null;
+
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
+
     class Node<T> {
         T data;
         Node<T> prev;
