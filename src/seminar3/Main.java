d5eeda3c2d4074ae19da541c8101fea7ed10030f
diniles <dzliashko@gmail.com>
2023-08-23 12:38:07 +0300

    Feat: seminar 3 class Main

---
+++ src/seminar3/Main.java
@@ -0,0 +1,25 @@
+package seminar3;
+
+public class Main {
+    public static void main(String[] args) {
+        Band hellowen = new Band("Helloween", 1983);
+        Band ironMaiden = new Band("Iron Maiden", 1975);
+        Band manowar = new Band("Manowar", 1980);
+        Band motorhead = new Band("Motörhead", 1975);
+
+        DoubleLinkedList<Band> bandsList = new DoubleLinkedList<>();
+        bandsList.addToStart(hellowen);
+        bandsList.addToStart(ironMaiden);
+        bandsList.addToEnd(manowar);
+        bandsList.addToEnd(motorhead);
+
+        System.out.println("Original list");
+        System.out.println(bandsList.toString());
+        System.out.println();
+
+        bandsList.reverse();
+
+        System.out.println("Reversed list");
+        System.out.println(bandsList.toString());
+    }
+}
