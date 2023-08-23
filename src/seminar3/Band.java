ebcc6d5973f6a5d238b71dd71125b5dcf0a591cb
diniles <dzliashko@gmail.com>
2023-08-23 12:21:36 +0300

    Feat: seminar 3 class Band

---
+++ src/seminar3/Band.java
@@ -0,0 +1,27 @@
+package seminar3;
+
+public class Band {
+    private String bandName;
+    private int formedDate;
+
+    public Band(String bandName, int date) {
+        this.bandName = bandName;
+        this.formedDate = date;
+    }
+
+    public String getBandName() {
+        return bandName;
+    }
+
+    public void setBandName(String bandName) {
+        this.bandName = bandName;
+    }
+
+    public int getFormedDate() {
+        return formedDate;
+    }
+
+    public void setFormedDate(int formedDate) {
+        this.formedDate = formedDate;
+    }
+}
