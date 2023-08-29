5f13d0ff5376cf734c116cd674034abd12b33233
diniles <dzliashko@gmail.com>
2023-08-29 11:56:29 +0300

    Feat: seminar 4 class Main

--- src/seminar4/Main.java
+++ src/seminar4/Main.java
@@ -6,5 +6,33 @@ public class Main {
         System.out.println(employee1.hashCode());
         Employee employee2 = new Employee("AAAA", 31);
         System.out.println(employee2.hashCode());
+
+        HashMap<String, String> hashMap = new HashMap<>(4);
+
+        String oldValue;
+        oldValue = hashMap.put("+79007774431", "AAAAAAA");
+        oldValue = hashMap.put("+79007774432", "BBBBBBB");
+        oldValue = hashMap.put("+79007774431", "CCCCCCC1");
+        oldValue = hashMap.put("+79007774433", "CCCCCCC2");
+        oldValue = hashMap.put("+79007774434", "CCCCCCC3");
+        oldValue = hashMap.put("+79007774435", "CCCCCCC4");
+        oldValue = hashMap.put("+79007774436", "CCCCCCC5");
+        oldValue = hashMap.put("+79007774437", "CCCCCCC6");
+        oldValue = hashMap.put("+79007774438", "CCCCCCC7");
+        oldValue = hashMap.put("+79007774439", "CCCCCCC8");
+
+        String res = hashMap.get("+79007774435");
+        System.out.println(res);
+        res = hashMap.get("+79107774435");
+        System.out.println(res);
+
+        oldValue = hashMap.remove("+79007774435");
+        System.out.println(oldValue);
+        oldValue = hashMap.remove("+79007774435");
+        System.out.println(oldValue);
+
+        for (HashMap<String, String>.Entity element : hashMap) {
+            System.out.println(element.key);
+        }
     }
 }
